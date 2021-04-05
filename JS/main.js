fed142448244632c8483c53bce7f3dfce2bd9731
Ralf Prezia <ralfprezia@gmail.com>
2021-04-05 10:59:21 -0300

    js

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,21 @@
 var d = new Date();
-alert(d);
+alert("Olá tudo bem com você?\nSinta-se em casa.\n\nMeu nome é Ralf Prézia.\nMuito obrigado por acessar o meu currículo.\n\n " + d);
+
+function trocar(elemento)
+{
+    elemento.innerHTML = "Clique nas setas para a navegação";
+    //document.getElementById("mousemove").innerHTML = "Confirmando mousemove!!!";
+    //alert("trocar texto");
+    
+}
+
+function voltar(elemento)
+{
+    //elemento.innerHTML = "Passe o mouse aqui!";
+    document.getElementById("mousemove").innerHTML ="Seja muito bem vindo(a)";
+}
+
+function clicou(elemento)
+{
+    window. history. back();
+}
